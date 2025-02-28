873bf66f47b27f51f0a347d1f0051a086b2a70fd
EnzoPENISSON <enzopenisson25@orange.fr>
2025-02-28 09:49:43 +0100

    Adherent Service & Adherent controller

--- src/main/java/fr/enzop/services/BookService.java
+++ src/main/java/fr/enzop/services/BookService.java
@@ -67,7 +67,6 @@ public class BookService {
         return bookRepository.save(book);
     }
 
-    //addBook
     public Book updateBook(BookRequest request, Integer id) {
         if (request == null || !request.paramsSet()) {
             throw new MissingParameterException();
